0b732e71ae95b78479ac3c64ff7eaac5629ffca6
Shahadat_Robin <sh.robin025@gmail.com>
2022-11-08 20:02:44 +0600

    test

---
+++ sortStringByCount.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+    char str[50];  //bbabbaccddd
+    scanf("%s",str);
+
+    int s_length = strlen(str);
+
+    char arr[s_length][s_length]; //['bb']
+
+    for(int i=0;i<s_length;i++){
+        int asc = str[i];
+
+    }
+
+    return 0;
+}
