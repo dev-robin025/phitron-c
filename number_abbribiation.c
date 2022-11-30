42a0034f41f81122583521364ab00e680799126f
Shahadat_Robin <sh.robin025@gmail.com>
2022-11-30 22:11:21 +0600

    updated

---
+++ number_abbribiation.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+
+float addAbbreviation(float val, char abb)
+{
+}
+
+int main()
+{
+    long long int n;
+    scanf("%lld", &n);
+
+    float res;
+    if (n >= 1e6)
+    {
+        res = n / 1e6;
+    }
+
+    return 0;
+}
